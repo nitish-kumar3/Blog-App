c4717889c4aaf31f456e9b481fd372839e94c8b6
web-dev <nitish6206774025@gmail.com>
2025-02-10 00:14:07 +0530

    Completed

---
+++ backend/routes/user.route.js
@@ -0,0 +1,21 @@
+import express from "express";
+import {
+  getAdmins,
+  getMyProfile,
+  login,
+  logout,
+  register,
+} from "../controller/user.controller.js";
+import { isAuthenticated } from "../middleware/authUser.js";
+
+const router = express.Router();
+
+router.post("/register", register);
+// router.route("/register").post(register); //secound method to making route
+router.post("/login", login);
+router.get("/logout", isAuthenticated, logout);
+// router.get("/my-profile", getMyProfile);
+router.get("/my-profile", isAuthenticated, getMyProfile);
+router.get("/admins", getAdmins);
+
+export default router;
