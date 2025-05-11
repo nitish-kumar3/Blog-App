c26d7e57da3dfaf456c5e793f4fb081e7df61645
web-dev <nitish6206774025@gmail.com>
2025-05-11 23:14:29 +0530

    done

--- backend/jwt/AuthToken.js
+++ backend/jwt/AuthToken.js
@@ -6,15 +6,16 @@ const createTokenAndSaveCookies = async (userId, res) => {
     expiresIn: "30d",
   });
   res.cookie("jwt", token, {
-    httpOnly: true, // Temporarily set to false for testing //deploye se phle
+    // httpOnly: true, // Temporarily set to false for testing //deploye se phle
 
     // first step me package.json me nodemon ke node likhe (node index.js) or fir gitigonre banaye aur env and module ko isme likhe third step me yeha aaye httpOnly ko false kare aur sameSite ko none kare
-    // httpOnly: false, //deploye ke liye , false isliye kiye h kyuki frontend or backend dono alag alag domain pe h
+    httpOnly: false, //deploye ke liye , false isliye kiye h kyuki frontend or backend dono alag alag domain pe h
 
-    secure: false,
-    sameSite: "lax",
-    // sameSite: "none",  //none isliye kiye h kyuki frontend or backend dono alag alag domain pe h
-    path: "/", // Ensure the cookie is available throughout the site
+    // secure: false,
+    secure: true,
+    // sameSite: "lax",
+    sameSite: "none",  //none isliye kiye h kyuki frontend or backend dono alag alag domain pe h
+    // path: "/", // Ensure the cookie is available throughout the site
   });
   await User.findByIdAndUpdate(userId, { token });
   return token;
