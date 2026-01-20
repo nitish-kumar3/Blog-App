8b837fca1faba3138b7f52357966a054b52db2b7
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2026-01-20 20:19:46 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,6 @@ import { FaGithub } from "react-icons/fa";
 import { BsYoutube } from "react-icons/bs";
 import { FaLinkedin } from "react-icons/fa";
     
-
 const Footer = () => {
   return (
     <>
