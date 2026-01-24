76764ee80e095c215219616529ba758ded7fbb1d
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2026-01-24 10:18:18 +0530

    Update AuthToken.js

--- backend/jwt/AuthToken.js
+++ backend/jwt/AuthToken.js
@@ -1,5 +1,7 @@
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
+
+    
       
 const createTokenAndSaveCookies = async (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
