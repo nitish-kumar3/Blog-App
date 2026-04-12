5a640f18f220b99a8099efe14134f2eebdf725aa
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2026-04-13 00:14:24 +0530

    Update Creator.jsx

--- frontend/src/Home/Creator.jsx
+++ frontend/src/Home/Creator.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { BACKEND_URL } from "../utils/utils";
 
-                         
+                        
 function Creator() {
   const [admin, setAdmin] = useState([]);
   console.log(admin);
