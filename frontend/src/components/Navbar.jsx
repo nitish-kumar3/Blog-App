6c8e509fd87e4f04134dc0a183e927f816659dcd
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2026-08-05 08:01:58 +0530

    Add spacing between imports in Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,6 +7,9 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import { BACKEND_URL } from "../utils/utils";
 
+
+
+
 function Navbar() {
   const [show, setShow] = useState(false);
 
